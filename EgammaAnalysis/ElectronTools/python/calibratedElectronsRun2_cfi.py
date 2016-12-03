34327fd6063ea2b4cae079872211a8c9dbe0c4f6
gfasanel <giuseppe.fasanella@cern.ch>
2016-12-03 13:32:54 +0100

    adding first version Full 2016 corrections

--- EgammaAnalysis/ElectronTools/python/calibratedElectronsRun2_cfi.py
+++ EgammaAnalysis/ElectronTools/python/calibratedElectronsRun2_cfi.py
@@ -1,9 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
-correctionType = "80Xapproval"
+correctionType = "Moriond2017_JEC"
 files = {"Prompt2015":"EgammaAnalysis/ElectronTools/data/ScalesSmearings/74X_Prompt_2015",
          "76XReReco" :"EgammaAnalysis/ElectronTools/data/ScalesSmearings/76X_16DecRereco_2015_Etunc",
-         "80Xapproval" : "EgammaAnalysis/ElectronTools/data/ScalesSmearings/80X_ichepV1_2016_ele"}
+         "80Xapproval" : "EgammaAnalysis/ElectronTools/data/ScalesSmearings/80X_ichepV1_2016_ele",
+         "Moriond2017_JEC" : "EgammaAnalysis/ElectronTools/data/ScalesSmearings/Winter_2016_reReco_v1_ele" #only to derive JEC correctionsb
+         }
 
 calibratedElectrons = cms.EDProducer("CalibratedElectronProducerRun2",
 
